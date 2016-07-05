3bf1d6aa37ba6e574f7a17ced6a7f95a62e6e489
James Goppert <james.goppert@gmail.com>
2016-07-05 12:13:06 -0400

    Updated 0.3.4

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 root=$PWD
-set -e
-export PATH=$HOME/anaconda3/bin:$PATH
-conda install conda-build anaconda-client anaconda-build
-python --version
-bdir=~/anaconda3/conda-bld
-rm -rf $bdir
-rm -rf dist
-conda config --set anaconda_upload no
-conda build .
-conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/anaconda3/conda-bld
-anaconda upload -u dronecrew $bdir/linux-64/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/linux-32/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/win-32/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/win-64/px4tools*.tar.bz2
-anaconda upload -u dronecrew $bdir/osx-64/px4tools*.tar.bz2
-# anaconda build submit --tail .
+
+do_build ()
+{
+	set -e
+	version=$1
+	export PATH=$HOME/$version/bin:$PATH
+	conda install conda-build anaconda-client anaconda-build
+	python --version
+	bdir=~/$version/conda-bld
+	rm -rf $bdir
+	rm -rf dist
+	conda config --set anaconda_upload no
+	conda build .
+	conda convert --platform all $bdir/linux-64/px4tools*.tar.bz2 -o ~/$version/conda-bld
+
+	anaconda upload -u dronecrew $bdir/linux-64/px4tools*.tar.bz2 || true
+	anaconda upload -u dronecrew $bdir/linux-32/px4tools*.tar.bz2 || true
+	anaconda upload -u dronecrew $bdir/win-32/px4tools*.tar.bz2 || true
+	anaconda upload -u dronecrew $bdir/win-64/px4tools*.tar.bz2 || true
+	anaconda upload -u dronecrew $bdir/osx-64/px4tools*.tar.bz2 || true
+	# anaconda build submit --tail .
+}
+
+do_build anaconda2
+do_build anaconda3
 
 #cd $root
 #python setup.py sdist upload
